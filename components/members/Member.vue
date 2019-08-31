49d422d33f8584bc9ba288abcc29e16759b888d6
Nicole <nicole929chan@gmail.com>
2019-08-31 10:48:29 +0000

    fix, 呈現的內容

--- components/members/Member.vue
+++ components/members/Member.vue
@@ -4,7 +4,7 @@
       <div class="media">
         <div class="media-left">
           <figure class="image is-48x48">
-            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+            <img :src="member.logo" :alt="member.name">
           </figure>
         </div>
         <div class="media-content">
@@ -14,17 +14,15 @@
             </nuxt-link>
           </p>
           <p class="subtitle is-6">
-            @johnsmith
+            {{ member.email }}
           </p>
         </div>
       </div>
 
       <div class="content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-        <a href="#">#css</a> <a href="#">#responsive</a>
+        {{ member.address }}
+        <div>{{ member.phone_number }}</div>.
         <br>
-        <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
       </div>
     </div>
   </div>
--- pages/members/_id.vue
+++ pages/members/_id.vue
@@ -2,7 +2,7 @@
   <div class="section">
     <div class="container is-fluid">
       <div>
-        <img :src="image" alt="">
+        <img :src="member.activity.image_path" alt="">
         <section>
           {{ member.activity.description }}
         </section>
@@ -25,11 +25,6 @@ import Add from '@/components/plan/Add'
 export default {
   components: {
     Add
-  },
-  computed: {
-    image () {
-      return `http://shop.test/storage/${this.member.activity.image_path}`
-    }
   },
   async asyncData ({ params, app }) {
     const response = await app.$axios.$get(`members/${params.id}`)
